4958f149f25b9e779db723a54166a13bc8c752ae
SEANGLAY <devolutionary.wizard@gmail.com>
2024-07-20 17:27:38 +0700

    feat: sign in

--- authentication/src/index.ts
+++ authentication/src/index.ts
@@ -1,12 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { JWT_EXPIRATION, PORT } from './core/config';
+import { PORT } from './core/config';
 import { signUpSchema } from './types/user.type';
 import { validateData } from './middleware/validator.middleware';
 import { logger } from './utils/logger';
 import { errorHandler } from './middleware/error-handling.middleware';
 import { morganMiddleware } from './middleware/morgan.middleware';
-import { createUser } from './repository/user.repository';
-import { generatePassword, signToken } from './services/auth.service';
+import { createUser, findUserByEmail } from './repository/user.repository';
+import { generatePassword, signToken, validPassword } from './services/auth.service';
 
 const app = express();
 
@@ -19,7 +19,27 @@ app.get('/', (_req: Request, res: Response) => res.json({ message: 'Hello World!
 app.post('/sign-up', validateData(signUpSchema), async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password } = req.body;
-        await createUser({ password: generatePassword(password), ...req.body });
+        const hashPassword = generatePassword(password);
+        await createUser({ email, password: hashPassword });
+        const payload = {
+            email: email,
+            iat: Math.floor(Date.now() / 1000),
+        };
+        const token = signToken(payload);
+        res.status(201).json({ token });
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.post('/sign-in', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        const user = await findUserByEmail(email);
+        if (!user) res.status(404).json({ message: 'User not found' });
+
+        if (!validPassword(password, user.password)) res.status(401).json({ message: 'Invalid password' });
+
         const payload = {
             email: email,
             iat: Math.floor(Date.now() / 1000),
--- authentication/src/repository/user.repository.ts
+++ authentication/src/repository/user.repository.ts
@@ -2,3 +2,7 @@ import knex from "../db/knex";
 import { SignUpType } from "../types/user.type";
 
 export const createUser = (user: SignUpType) => knex('users').insert(user);
+
+export const findUserByEmail = (email: string) => knex('users')
+    .where({ email })
+    .first();
